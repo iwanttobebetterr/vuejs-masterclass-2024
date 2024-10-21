221ff2412710bd63b21b74fdb42f041eacdb476b
AnhPH <anhph.dev@gmail.com>
2024-10-21 16:32:44 +0700

    feat: Lesson 42 Create Layout Vue.js Component

---
+++ src/components/layout/SidebarLinks.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface Link {
+  title: string
+  icon: string
+  to: string
+}
+
+defineProps<{
+  links: Link[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="link in links"
+    :key="link.title"
+    :to="link.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+    exactActiveClass="text-primary bg-muted"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+</template>
